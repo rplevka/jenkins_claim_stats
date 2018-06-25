994d128e8e55d97ad67249af8fa5a1843338452d
Jan Hutar <jhutar@redhat.com>
2018-06-25 13:54:57 +0200

    Format it in a same way as elsewhere

--- unclaimed.py
+++ unclaimed.py
@@ -5,4 +5,4 @@ import claims
 reports = [i for i in claims.Report() if i['status'] in claims.Case.FAIL_STATUSES and not i['testActions'][0].get('reason')]
 
 for r in reports:
-    print(u'{0} {1} {2}'.format(r['distro'], r['className'], r['name']))
+    print(u'{0} {1}::{2}'.format(r['distro'], r['className'], r['name']))
