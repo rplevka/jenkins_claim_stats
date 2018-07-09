4d352a03aa46e199b7c24d7591e730cc6109aa17
Jan Hutar <jhutar@redhat.com>
2018-07-09 10:40:12 +0200

    Some useful code

--- claimable.py
+++ claimable.py
@@ -6,4 +6,10 @@ import claims
 report = claims.Report()
 rules = claims.Ruleset()
 
+#report = [r for r in report if r['name'] == 'test_positive_create_with_puppet_class_id']
+#rules = [r for r in rules if r['reason'] == 'https://github.com/SatelliteQE/robottelo/issues/6115']
+
 claims.claim_by_rules(report, rules, dryrun=True)
+
+#for case in [i for i in report if i['status'] in claims.Case.FAIL_STATUSES]:
+#    print(case['url'])
