2ea66fcde3c5e793f51fc11d964fc695d012d8c3
Jan Hutar <jhutar@redhat.com>
2018-07-10 21:40:20 +0200

    Fixed tests

--- test_claims.py
+++ test_claims.py
@@ -3,41 +3,44 @@
 
 import claims
 
-checkme = {
-    'greeting': 'Hello world',
-    'area': 'IT Crowd',
-}
+def test_rule_matches():
+    checkme = {
+        'name': 'test',
+        'greeting': 'Hello world',
+        'area': 'IT Crowd',
+    }
+    result = claims.Case(checkme)
 
-assert claims.rule_matches(checkme, {'field': 'greeting', 'pattern': 'Hel+o'}) == True
-assert claims.rule_matches(checkme, {'field': 'greeting', 'pattern': 'This is not there'}) == False
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}]}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}]}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}]}) == False
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'This is not there'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'This is not there'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'OR': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'IT'}]}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'IT'}]}]}) == True
-assert claims.rule_matches(checkme, {'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'This is not there'}]}]}) == False
-assert claims.rule_matches(checkme, {'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'OR': [{'field': 'greeting', 'pattern': 'Hel*o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
-assert claims.rule_matches(checkme, {'AND': [{'OR': [{'field': 'greeting', 'pattern': 'Hel*o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'IT'}]}) == True
-assert claims.rule_matches(checkme, {'AND': [{'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'field': 'greeting', 'pattern': 'Hel+o'}) == True
+    assert result.matches_to_rule({'field': 'greeting', 'pattern': 'This is not there'}) == False
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}]}) == True
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}]}]}) == True
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}]}) == True
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}]}) == True
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}]}]}) == False
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}]}) == False
+    assert result.matches_to_rule({'AND': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}]}) == False
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}, {'field': 'area', 'pattern': 'This is not there'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'This is not there'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'OR': [{'AND': [{'field': 'greeting', 'pattern': 'Hel+o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'IT'}]}]}) == True
+    assert result.matches_to_rule({'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'IT'}]}]}) == True
+    assert result.matches_to_rule({'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'AND': [{'field': 'area', 'pattern': 'This is not there'}]}]}) == False
+    assert result.matches_to_rule({'OR': [{'AND': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'AND': [{'OR': [{'field': 'greeting', 'pattern': 'Hel*o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'This is not there'}]}) == False
+    assert result.matches_to_rule({'AND': [{'OR': [{'field': 'greeting', 'pattern': 'Hel*o'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'IT'}]}) == True
+    assert result.matches_to_rule({'AND': [{'OR': [{'field': 'greeting', 'pattern': 'This is not there'}, {'field': 'greeting', 'pattern': 'world'}]}, {'field': 'area', 'pattern': 'IT'}]}) == True
