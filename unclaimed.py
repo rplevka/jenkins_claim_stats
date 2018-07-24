63aca59aacca3b528e7a17fd687e8ec233b4a05e
Jan Hutar <jhutar@redhat.com>
2018-07-24 08:16:49 +0200

    Not needed now I hope

--- unclaimed.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import claims
-
-reports = [i for i in claims.Report() if i['status'] in claims.Case.FAIL_STATUSES and not i['testActions'][0].get('reason')]
-
-for r in reports:
-    print(u'{0} {1}::{2}'.format(r['distro'], r['className'], r['name']))
